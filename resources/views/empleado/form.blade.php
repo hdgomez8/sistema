797eca49efa4edd9b8b2e99e90864fd76bd6ac59
hdgomez8 <31425829+hdgomez8@users.noreply.github.com>
2021-10-01 15:51:54 -0500

    1
    1

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -2,24 +2,24 @@ Datos en comun entre create y edit
 <br>
 
 <label for="Nombre">Nombre</label>
-<input type="text" name="Nombre" value="{{isset($empleado->Nombre)?$empleado->Nombre:'';}}" id="Nombre">
+<input type="text" name="Nombre" value="{{isset($empleado->Nombre)?$empleado->Nombre:''}}" id="Nombre">
 <br>
 
 <label for="ApellidoPaterno">Apellido Paterno</label>
-<input type="text" name="ApellidoPaterno" value="{{isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:'';}}" id="ApellidoPaterno">
+<input type="text" name="ApellidoPaterno" value="{{isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:''}}" id="ApellidoPaterno">
 <br>
 
 <label for="ApellidoMaterno">Apellido Materno</label>
-<input type="text" name="ApellidoMaterno" value="{{isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:'';}}" id="ApellidoMaterno">
+<input type="text" name="ApellidoMaterno" value="{{isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:''}}" id="ApellidoMaterno">
 <br>
 
 <label for="Correo">Correo</label>
-<input type="text" name="Correo" value="{{isset($empleado->Correo)?$empleado->Correo:'';}}" id="Correo">
+<input type="text" name="Correo" value="{{isset($empleado->Correo)?$empleado->Correo:''}}" id="Correo">
 <br>
 
 <label for="Foto"></label>
-{{isset($empleado->Correo)?$empleado->Correo:'';}}
-<img src="{{asset('storage').'/'.$empleado->Foto}}" alt="">
+{{isset($empleado->Foto)?$empleado->Foto:''}}
+<img src="{{asset('storage').'/'.(isset($empleado->Foto)?$empleado->Foto:'')}}" alt="">
 <input type="file" name="Foto" id="Foto">
 <br>
 
